a978988b172db5541bd88b2c85be3b004f862bce
Dazzler123 <dasinduhewagamage@gmail.com>
2022-10-24 16:12:50 +0530

    Inheritance example added

--- Inheritance.js
+++ Inheritance.js
@@ -6,13 +6,33 @@
 //     id:'C001'
 // };
 
-function Vehicle() {
+// function Vehicle() {
+//
+// }
+//
+// function Car() {
+//
+// }
+//
+// Vehicle.prototype.model="CLA AMG 200";
+// Vehicle.prototype.year=2022;
 
+class CustomerDTO {
+    constructor(id,name,address,salary) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+    }
 }
 
-function Car() {
+let b1 = new CustomerDTO("C001","Dasun","Wattala","15000.00");
 
+class CustomerOne extends CustomerDTO {
+    constructor(id,name,address,salary) {
+        super(id,name,address,salary);
+    }
 }
 
-Vehicle.prototype.model="CLA AMG 200";
-Vehicle.prototype.year=2022;
+let b2 = new CustomerOne("C002","Kasun","Panadura","25000.00")
+
