50df9aeeb80e0d9f95188681a5bb2db71b3ef7a7
Dazzler123 <dasinduhewagamage@gmail.com>
2022-10-24 15:51:41 +0530

    Content updated

---
+++ Inheritance.js
@@ -0,0 +1,18 @@
+// function Vehicle() {
+//
+// }
+//
+// var c = {
+//     id:'C001'
+// };
+
+function Vehicle() {
+
+}
+
+function Car() {
+
+}
+
+Vehicle.prototype.model="CLA AMG 200";
+Vehicle.prototype.year=2022;
